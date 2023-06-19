a7ed71d2e32738d9f6d44dc35979ca4b6c6eec75
Navneet <navneet.prajapat.work@gmail.com>
2023-06-19 15:12:36 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  // const [hr, setHr] = useState(0);
+  // const [min, setMin] = useState(0);
+  const [sec, setSec] = React.useState(0);
+  // setSec(sec + 1);
+
+  // function Timerstart(){
+
+  // }
+
+  setInterval(() => {
+    setSec(sec + 1);
+  }, 1000);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {sec}
     </div>
-  );
+  )
+
+
+
+  // setInterval(() => {
+  //   setSec(sec+1);
+  //   // if(sec===60){
+  //   //   setSec(0);
+  //   //   setMin(min+1);
+  // //     if(min===60){
+  // // setMin(0);
+  // // setHr(hr+1);
+  // //     }
+  //   }
+  // }, 1000);
+
+  // const [variable, function] = useState(variable = value) 
+
+
+  // return (
+  //   <div className="App">
+  //     {/* <div className='hr'>
+  //       {hr}:
+  //     </div>
+  //     <div className='min'>
+  //       {min}:
+  //     </div> */}
+  //     <div className='sec'>
+  //       {sec}
+  //     </div>
+  //     {/* <div className='button'>
+  //       <button onClick={Timerstart} > Start</button>
+  //     </div> */}
+  //   </div>
+
+  // );
 }
 
 export default App;
